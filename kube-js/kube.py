b4b8943cfa97cf1ddcd0ebb04f790fadc8f06793
Lalit Yadav <69167025+yadav12122000@users.noreply.github.com>
2021-07-01 14:17:01 +0530

    Add files via upload

---
+++ kube-js/kube.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+import cgi
+import subprocess
+
+form = cgi.FieldStorage()
+cmd = "sudo" + " " + form.getvalue("x")
+op = subprocess.getoutput(cmd)
+print(op)
